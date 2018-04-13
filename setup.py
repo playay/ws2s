466a070c0c94f13594bd661877c04dce771a77dc
chenyan <chenyan@wecash.net>
2018-04-13 17:30:21 +0800

    setup.py install_requires  add sshtunnel>=0.1.3

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.1.2',  # Required
+    version='2.1.3',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -123,7 +123,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['gevent>=1.2.2'],  # Optional
+    install_requires=['gevent>=1.2.2', 'sshtunnel>=0.1.3'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
